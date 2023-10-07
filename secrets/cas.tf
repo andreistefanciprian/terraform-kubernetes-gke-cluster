a97b13c0d14c70541a09f04f011e3b27f10b17fc
Stefan Andrei <andreistefanciprian@gmail.com>
2023-10-07 19:16:43 +1030

    cert-manager Google CAS Issuer

---
+++ secrets/cas.tf
@@ -0,0 +1,94 @@
+# Infrastructure used by cert-manager Google CAS Issuer: https://github.com/andreistefanciprian/flux-demo/tree/main/infra/cert-manager
+
+# Enable Certificate Authority Service API
+resource "google_project_service" "privateca" {
+  service            = "privateca.googleapis.com"
+  disable_on_destroy = true
+}
+
+resource "google_privateca_ca_pool" "default" {
+  name     = "my-pool"
+  location = var.gcp_region
+  tier     = "ENTERPRISE"
+  publishing_options {
+    publish_ca_cert = true
+    publish_crl     = true
+  }
+  labels = {
+    foo = "bar"
+  }
+  depends_on = [google_project_service.privateca]
+}
+
+resource "google_privateca_certificate_authority" "quicksampleapp" {
+  // This example assumes this pool already exists.
+  // Pools cannot be deleted in normal test circumstances, so we depend on static pools
+  pool                     = "my-pool"
+  certificate_authority_id = "my-certificate-authority"
+  location                 = var.gcp_region
+  deletion_protection      = "true"
+  config {
+    subject_config {
+      subject {
+        organization = "quicksampleapp"
+        common_name  = "my-ca"
+      }
+      subject_alt_name {
+        dns_names = ["quicksampleapp.com"]
+      }
+    }
+    x509_config {
+      ca_options {
+        is_ca                  = true
+        max_issuer_path_length = 10
+      }
+      key_usage {
+        base_key_usage {
+          digital_signature  = true
+          content_commitment = true
+          key_encipherment   = false
+          data_encipherment  = true
+          key_agreement      = true
+          cert_sign          = true
+          crl_sign           = true
+          decipher_only      = true
+        }
+        extended_key_usage {
+          server_auth      = true
+          client_auth      = false
+          email_protection = true
+          code_signing     = true
+          time_stamping    = true
+        }
+      }
+    }
+  }
+  lifetime = "86400s"
+  key_spec {
+    # algorithm = "RSA_PKCS1_4096_SHA256"
+    algorithm = "EC_P384_SHA384"
+  }
+  depends_on = [google_privateca_ca_pool.default, google_project_service.privateca]
+}
+
+# Create a dedicated Google Service Account (SA) to be used by the CAS Issuer to access the Google Cloud CAS APIs
+resource "google_service_account" "cas-issuer" {
+  account_id   = "cas-issuer"
+  display_name = "This SA will be impersonated by a CAS K8s SA to issue certifictes."
+}
+
+# Grant k8s SA permission to impersonate Google SA via workload identity
+resource "google_service_account_iam_binding" "cas-service-account-iam" {
+  service_account_id = google_service_account.cas-issuer.name
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    "serviceAccount:${var.gcp_project}.svc.id.goog[cert-manager/cert-manager-google-cas-issuer]",
+  ]
+}
+resource "google_privateca_ca_pool_iam_binding" "binding" {
+  ca_pool = google_privateca_ca_pool.default.id
+  role    = "roles/privateca.certificateRequester"
+  members = [
+    "serviceAccount:${google_service_account.cas-issuer.email}",
+  ]
+}
